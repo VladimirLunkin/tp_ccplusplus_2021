75d6c631c51a89e5ff254dae21d03b59d53b09b6
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-03-11 21:21:16 +0300

    added tests film

--- test/film.cpp
+++ test/film.cpp
@@ -19,20 +19,8 @@ TEST(free_film, correct_arguments) {
     char movie_title[] = "Огонь";
     char genre[] = "драма";
     auto *film = (film_t*)malloc(sizeof(film_t));
-    if (film == nullptr) {
-        FAIL() << "can't allocate memory";
-    }
     film->movie_title = (char*)malloc(strlen(movie_title) + 1);
-    if (film->movie_title == nullptr) {
-        free(film);
-        FAIL() << "can't allocate memory";
-    }
     film->genre = (char*)malloc(strlen(genre) + 1);
-    if (film->genre == nullptr) {
-        free(film->movie_title);
-        free(film);
-        FAIL() << "can't allocate memory";
-    }
     ASSERT_EQ(0, free_film(film));
 }
 
@@ -55,49 +43,53 @@ TEST(create_film, correct_arguments) {
     free_film(film);
 }
 
-//TEST(create_film_str, null_arguments) {
-//    film_t *film = create_film_str(nullptr);
-//    EXPECT_EQ(nullptr, film);
-//    free_film(film);
-//}
-//TEST(create_film_str, wrong_arguments_1) {
-//    char data[] = "123";
-//    film_t *film = create_film_str(data);
-//    EXPECT_EQ(nullptr, film);
-//    free_film(film);
-//}
-//TEST(create_film_str, wrong_arguments_2) {
-//    char data[] = "Огонь 2020 драма";
-//    film_t *film = create_film_str(data);
-//    EXPECT_EQ(nullptr, film);
-//    free_film(film);
-//}
-//TEST(create_film_str, wrong_arguments_3) {
-//    char data[] = "Огонь Огонь Огонь Огонь";
-//    film_t *film = create_film_str(data);
-//    EXPECT_EQ(nullptr, film);
-//    free_film(film);
-//}
-//TEST(create_film_str, correct_arguments) {
-//    char data[] = "Огонь 2020 драма 7.7";
-//    film_t *film = create_film_str(data);
-//    EXPECT_TRUE(film != nullptr &&
-//                        strcmp(film->movie_title, "Огонь") == 0 &&
-//                        film->year_of_issue == 2020 &&
-//                        strcmp(film->genre, "драма") == 0 &&
-//                        is_equal(film->average_rating, 7.7));
-//    free_film(film);
-//}
+TEST(read_film_from_file, null_arguments) {
+    film_t *film = read_film_from_file(nullptr);
+    EXPECT_EQ(nullptr, film);
+    free_film(film);
+}
+TEST(read_film_from_file, empty_file) {
+    FILE *fp = fopen(SOURCE_DIR"/wrong_data/db_1", "r");
+    film_t *film = read_film_from_file(fp);
+    EXPECT_EQ(nullptr, film);
+    free_film(film);
+    fclose(fp);
+}
+TEST(read_film_from_file, wrong_data_1) {
+    FILE *fp = fopen(SOURCE_DIR"/wrong_data/db_2", "r");
+    film_t *film = read_film_from_file(fp);
+    EXPECT_EQ(nullptr, film);
+    free_film(film);
+    fclose(fp);
+}
+TEST(read_film_from_file, wrong_data_2) {
+    FILE *fp = fopen(SOURCE_DIR"/wrong_data/db_3", "r");
+    film_t *film = read_film_from_file(fp);
+    EXPECT_EQ(nullptr, film);
+    free_film(film);
+    fclose(fp);
+}
+TEST(read_film_from_file, correct_arguments) {
+    FILE *fp = fopen(SOURCE_DIR"/data/db_1", "r");
+    film_t *film = read_film_from_file(fp);
+    EXPECT_TRUE(film != nullptr &&
+                        strcmp(film->movie_title, "Огонь") == 0 &&
+                        film->year_of_issue == 2020 &&
+                        strcmp(film->genre, "драма") == 0 &&
+                        is_equal(film->average_rating, 7.7));
+    free_film(film);
+    fclose(fp);
+}
 
 TEST(print_film, null_arguments) {
     EXPECT_EQ(1, print_film(nullptr, nullptr));
 }
 TEST(print_film, file) {
     char file_name[] = "test_print_film";
-    FILE *fptr = fopen(file_name, "w");
+    FILE *fp = fopen(file_name, "w");
     film_t *film = create_film((char*)"Огонь", 2020, (char*)"драма", 7.7);
-    EXPECT_EQ(0, print_film(fptr, film));
-    fclose(fptr);
+    EXPECT_EQ(0, print_film(fp, film));
+    fclose(fp);
     remove(file_name);
     free_film(film);
 }
