cf94aa27060a3148b815ca076f1e40f22f10e4c5
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-03-14 23:18:59 +0300

    refactoring 3

--- include/list_of_films/list_of_films.h
+++ include/list_of_films/list_of_films.h
@@ -1,6 +1,6 @@
-#ifndef HW_1_LIST_OF_FILMS_H
-#define HW_1_LIST_OF_FILMS_H
+#ifndef INCLUDE_LIST_OF_FILMS_LIST_OF_FILMS_H_
+#define INCLUDE_LIST_OF_FILMS_LIST_OF_FILMS_H_
 
 int print_list_of_films(const char *, float, float, char *, unsigned short);
 
-#endif //HW_1_LIST_OF_FILMS_H
+#endif  // INCLUDE_LIST_OF_FILMS_LIST_OF_FILMS_H_
