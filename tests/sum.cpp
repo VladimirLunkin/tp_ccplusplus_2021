5544e9a64da8b1d63106af566105c671660ba477
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-03-19 16:35:42 +0300

    removed PARALLEL from travis

--- tests/sum.cpp
+++ tests/sum.cpp
@@ -14,8 +14,23 @@ TEST(sum, null_array) {
     ASSERT_EQ(0, sum(arr, 0));
 }
 
-TEST(sum, correct_arguments) {
-    int arr[100];
-    std::fill_n(arr, 100, 1);
-    ASSERT_EQ(99, sum(arr, 100));
+TEST(sum, array_size_100) {
+    const ssize_t size = 100;
+    int arr[size];
+    std::fill_n(arr, size, 1);
+    ASSERT_EQ(99, sum(arr, size));
+}
+
+TEST(sum, array_size_10000) {
+    const ssize_t size = 10000;
+    int arr[size];
+    std::fill_n(arr, size, 1);
+    ASSERT_EQ(9999, sum(arr, size));
+}
+
+TEST(sum, array_size_1000000) {
+    const ssize_t size = 1000000;
+    int arr[size];
+    std::fill_n(arr, size, 1);
+    ASSERT_EQ(999999, sum(arr, size));
 }
