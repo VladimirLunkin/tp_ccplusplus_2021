4057fe90336ebfc0bff623b8dceea7769844f4f0
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-03-19 15:18:50 +0300

    added tests

--- tests/sum.cpp
+++ tests/sum.cpp
@@ -7,3 +7,15 @@ extern "C" {
 TEST(sum, null_arguments) {
     ASSERT_EQ(0, sum(nullptr, 0));
 }
+
+TEST(sum, null_array) {
+    int arr[100];
+    std::fill_n(arr, 100, 0);
+    ASSERT_EQ(0, sum(arr, 0));
+}
+
+TEST(sum, correct_arguments) {
+    int arr[100];
+    std::fill_n(arr, 100, 1);
+    ASSERT_EQ(99, sum(arr, 100));
+}
