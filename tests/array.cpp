cdbbf45058fde410aadfae52ad259256cea6a98b
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-03-19 17:08:21 +0300

    added tests array

--- tests/array.cpp
+++ tests/array.cpp
@@ -4,6 +4,16 @@ extern "C" {
 #include "array.h"
 }
 
+bool cmp_arr(int *arr1, int *arr2, ssize_t size) {
+    for (int i = 0; i < size; ++i) {
+        if (arr1[i] != arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 TEST(free_arr, null_arguments) {
     ASSERT_EQ(1, free_arr(nullptr));
 }
@@ -12,7 +22,7 @@ TEST(free_arr, correct_arguments) {
     arr->capacity = 5;
     arr->arr = new int[arr->capacity];
     std::fill_n(arr->arr, arr->capacity, 13);
-    ASSERT_EQ(0, free_arr(nullptr));
+    ASSERT_EQ(0, free_arr(arr));
 }
 
 TEST(create_arr, null_arguments) {
@@ -39,11 +49,22 @@ TEST(arr_read_from_file, wrong_path) {
     const char file_name[] = "qwerty";
     ASSERT_EQ(nullptr, arr_read_from_file(file_name));
 }
-TEST(arr_read_from_file, correct_arguments) {
-    array_t *arr = create_arr(10);
+TEST(arr_read_from_file, miss_data_1) {
+    const char file_name[] = SOURCE_DIR"/data/miss_data_1";
+    ASSERT_EQ(nullptr, arr_read_from_file(file_name));
+}
+TEST(arr_read_from_file, miss_data_2) {
+    const char file_name[] = SOURCE_DIR"/data/miss_data_2";
+    ASSERT_EQ(nullptr, arr_read_from_file(file_name));
+}
+TEST(arr_read_from_file, data_1) {
+    const char file_name[] = SOURCE_DIR"/data/data_1";
+    array_t *arr = arr_read_from_file(file_name);
+    int expected_arr[] = {1, 2, 3, 4, 5};
 
     EXPECT_NE(nullptr, arr);
-    EXPECT_EQ(10, arr->capacity);
+    EXPECT_EQ(5, arr->capacity);
+    EXPECT_TRUE(cmp_arr(expected_arr, arr->arr, arr->capacity));
 
     free_arr(arr);
 }
