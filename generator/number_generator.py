32a1c0be2567c48925d1d4b977c605f6c7585288
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-03-19 14:38:30 +0300

    added number generator

---
+++ generator/number_generator.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+file = open("data.txt", "w")
+
+quantity = 100_000_000
+# quantity = 100
+max_num = 10_000
+
+file.write(str(quantity) + '\n')
+
+for i in range(quantity):
+    file.write(str(randint(-max_num, max_num)) + '\n')
+
+file.close()
